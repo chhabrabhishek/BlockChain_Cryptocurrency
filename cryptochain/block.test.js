7f0d8a9a6194b596673db49a2f47847413de127c
Abhishek Chhabra <abhishekchhabra@abhishek.Dlink>
2019-07-03 11:56:33 +0530

    Added genesis block along with test suite

--- cryptochain/block.test.js
+++ cryptochain/block.test.js
@@ -1,4 +1,6 @@
 const Block = require('./block');
+const {GENESIS_DATA} = require('./config')
+
 describe('Block', () => {
     const timeStamp = '03/07/2019';
     const lastHash = 'foo-lastHash';
@@ -12,4 +14,17 @@ describe('Block', () => {
         expect(block.hash).toEqual(hash);
         expect(block.data).toEqual(data);
     });
+
+    describe('genesis()', () => {
+        const genesisBlock = Block.genesis();
+
+        it('returns a block instance', () => {
+            expect(genesisBlock instanceof Block).toBe(true);
+        });
+
+        it('returns the GENESIS_DATA', () => {
+            expect(genesisBlock).toEqual(GENESIS_DATA);
+        });
+    });
+
 });
